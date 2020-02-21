8afc65113aa430331ca7c2e17082a4c62c6d00c7
Daniel Young <daniel.young@bluefrontier.co.uk>
2020-02-21 12:52:51 +0000

    fix: fixing async function

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const Config = require("./lib/Config");
 module.exports = new Config({
     errorExchangeName: "OOP_ERROR_EXCHANGE_NAME",
     jsonErrorQ: "OOP_JSON_ERROR_Q",
-    queuePrefetchCount: "OOP_QUEUE_PREFETCH_LIMIT",
+    queuePrefetchCount: "OOP_QUEUE_PREFETCH_LIMIT"
 });
